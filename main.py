e35ef2666b6ac4676a06c9e6234dfef5afafec97
Varun2855 <varunp2805@gmail.com>
2026-03-25 22:32:09 +0530

    readme and ai3

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from database import engine
 import models
 from auth.router import router as auth_router
 from notes.router import router as notes_router
+from ai.router import router as ai_router
 
 
 app=FastAPI()
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 app.include_router(notes_router)
+app.include_router(ai_router)
